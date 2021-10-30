49f6b0fdd6e7b2a0302433024bcc12faeec5ea50
dandara-navarro <dandaranavarro@gmail.com>
2021-10-29 21:36:41 -0700

    Restructuring the project

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,8 +1,8 @@
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
-function ExpenseList(props) {
+const ExpenseList = (props) => {
     return (
         <Card className="expenses">
             <ExpenseItem title={props.expenses[0].title}  amount={props.expenses[0].amount} date={props.expenses[0].date} />
